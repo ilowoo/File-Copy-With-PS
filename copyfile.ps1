c0970d7fe8505f1d3ac2e35bab8746f5928c9d7f
phyoewaipaing <phyoepaing3.142@gmail.com>
2023-04-11 21:14:45 +0630

    Update copyfile.ps1
    Changes : Line of $SourcePath omitted (line 8, 26). No need "-rescurse" for file listing (line 9,27). File filtering is done with "where" and "-match" (line 10,28)
    To improve: The outer if/else loop can be consolidated as one ( line 6, 23).

--- copyfile.ps1
+++ copyfile.ps1
@@ -1,21 +1,40 @@
 $N_Days_Back_to_Include = Read-Host "Which File of the Day: "
-$SourceDir = "Sources"
-$DestDir = "Destination"
+$SourceDir = "C:\CopyOperation\Sources"
+$DestDir = "C:\CopyOperation\Destination"
 $Year = Get-Date -Format "yyyy"
 $Today = Get-Date -Format "MMdd"
 if ($N_Days_Back_to_Include -eq 0){
-    $SourcePath = Get-ChildItem "$SourceDir" -Filter "$Today" -Directory -Recurse | %{$_.FullName}
-    $FileName = Get-ChildItem -Path "$SourcePath" -Name -Recurse | Select-String -Pattern "$Today"
-    if (!(Test-Path -Path "$DestDir/$Year/$Today")){
-        New-Item "$DestDir/$Year/$Today" -Type Directory
+
+    #$SourcePath = Get-ChildItem "$SourceDir" -Filter "$Today" -Directory -Recurse | %{$_.FullName}
+    #$FileName = Get-ChildItem -Path "$SourcePath" -Name -Recurse | Select-String -Pattern "$Today"
+    $FileName = Get-ChildItem -Path "$SourceDir\$Year\$Today" | where { $_.Name -match  "CRC_Opex_$($Year)$($Today)_54321.txt" }
+
+    if (!(Test-Path -Path "$DestDir\$Year\$Today")){
+        New-Item "$DestDir\$Year\$Today" -Type Directory
     }
-    Copy-Item -Path "$SourcePath/$FileName" -Destination "$DestDir/$Year/$Today/"
-}else{
+    
+    #Copy-Item -Path "$SourcePath\$FileName" -Destination "$DestDir\$Year\$Today\"
+    If ($FileName)
+    {
+    Copy-Item -Path $($FileName.FullName) -Destination "$DestDir\$Year\$Today\"
+    }
+}
+
+else{
     $BackDate = (Get-Date).AddDays("$N_Days_Back_to_Include").ToString("MMdd")
-    $SourcePath = Get-ChildItem "$SourceDir" -Filter "$BackDate" -Directory -Recurse | %{$_.FullName}
-    $FileName = Get-ChildItem -Path "$SourcePath" -Name -Recurse | Select-String -Pattern "$BackDate"
-    if (!(Test-Path -Path "$DestDir/$Year/$BackDate")){
-        New-Item "$DestDir/$Year/$BackDate" -Type Directory
+    
+    #$SourcePath = Get-ChildItem "$SourceDir" -Filter "$BackDate" -Directory -Recurse | %{$_.FullName}
+    #$FileName = Get-ChildItem -Path "$SourcePath" -Name -Recurse | Select-String -Pattern "$BackDate"
+    $FileName = Get-ChildItem -Path "$SourceDir\$Year\$BackDate" | where { $_.Name -match  "CRC_Opex_$($Year)$($BackDate)_54321.txt" }
+
+    if (!(Test-Path -Path "$DestDir\$Year\$BackDate")){
+        New-Item "$DestDir\$Year\$BackDate" -Type Directory
     }
-    Copy-Item -Path "$SourcePath/$FileName" -Destination "$DestDir/$Year/$BackDate/"
+    
+    #Copy-Item -Path "$SourcePath\$FileName" -Destination "$DestDir\$Year\$BackDate\"
+    If ($FileName)
+    {
+    Copy-Item -Path $($FileName.FullName) -Destination "$DestDir\$Year\$BackDate\"
+    }
+
 }
